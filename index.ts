2b9e963e36a1c4984e65db52590b96609d8c069b
Uice Lu <uicestone@gmail.com>
2019-07-31 01:25:02 +0800

    Initial commit

---
+++ index.ts
@@ -0,0 +1,79 @@
+import dgram from "dgram";
+import { Socket, AddressInfo } from "net";
+import { parseData } from "./wiegand-control/utils";
+import WgCtl from "./wiegand-control/WgCtl";
+
+const localPort = 6000;
+const remotePort = 8003;
+const remoteHost = "localhost";
+
+const controllerBySerial: { [serial: number]: WgCtl } = {};
+
+const socket = dgram.createSocket("udp4"); // local network using udp
+const client = new Socket(); // remote network using tcp
+
+socket.on("error", err => {
+  console.log(`[DMN] Socket error:\n${err.stack}.`);
+  socket.close();
+});
+
+socket.on("message", (msg, rinfo) => {
+  console.log(
+    `[DMN] Socket got local message from ${rinfo.address}:${rinfo.port}. \n`,
+    parseData(msg)
+  );
+  client.write(msg, err => {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+  });
+});
+
+socket.on("listening", () => {
+  const address = socket.address() as AddressInfo;
+  console.log(`[DMN] Socket listening ${address.address}:${address.port}.`);
+});
+
+socket.bind(localPort);
+
+client.connect(remotePort, remoteHost);
+
+client.on("connect", () => {
+  const address = client.remoteAddress;
+  const port = client.remotePort;
+  console.log(`[DMN] Socket connected to ${address}:${port}.`);
+});
+
+client.on("close", () => {
+  console.log(`[DMN] Socket closed. Reconnect after 1 second.`);
+  setTimeout(() => {
+    client.connect(remotePort, remoteHost);
+  }, 1000);
+});
+
+client.on("error", err => {
+  console.error(`[DMN] Socket error: ${err.message}.`);
+});
+
+client.on("data", async data => {
+  console.log(`[DMN] Socket got remote data\n`, data);
+  const parsedData = parseData(data);
+  const serial = parsedData.serial;
+  if (!serial) {
+    client.destroy(new Error("No controller serial could be parsed."));
+  }
+  let controller = controllerBySerial[serial];
+  if (!controller) {
+    controller = new WgCtl(socket, parsedData.serial);
+    await controller.detected;
+    controllerBySerial[serial] = controller;
+  }
+  controller.sendData(data.readUInt8(1), data.slice(8));
+});
+
+// setTimeout(async () => {
+//   const controllers = serials.map(serial => new WgCtl(socket, serial));
+//   await Promise.all(controllers.map(ctl => ctl.detected));
+//   controllers.map(c => c.getServerAddress());
+// }, 1000);
