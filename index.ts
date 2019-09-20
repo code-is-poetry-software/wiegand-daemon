f38529625f6f65c1abd4ee820849a1276de453c8
Uice Lu <uicestone@gmail.com>
2019-09-21 01:43:31 +0800

    search positively on start

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import dgram from "dgram";
-import { Socket, AddressInfo } from "net";
+import dgram, { Socket as UdpSocket } from "dgram";
+import { Socket as TcpSocket, AddressInfo } from "net";
 import WgCtl, { parseData } from "wiegand-control";
 import getLocalIp from "./utils/getLocalIp";
 import env from "dotenv";
@@ -16,7 +16,7 @@ const remoteHost = process.env.REMOTE_HOST || "api.kangazone.com";
 const controllerBySerial: { [serial: number]: WgCtl } = {};
 
 const socket = dgram.createSocket("udp4"); // local network using udp
-const client = new Socket(); // remote network using tcp
+const client = new TcpSocket(); // remote network using tcp
 
 socket.on("error", err => {
   console.log(`[DMN] Socket error:\n${err.stack}.`);
@@ -24,31 +24,43 @@ socket.on("error", err => {
 });
 
 socket.on("message", (msg, rinfo) => {
+  const message = parseData(msg);
   console.log(
     `[DMN] Socket got local message from ${rinfo.address}:${rinfo.port}. \n`,
-    parseData(msg)
+    message
   );
-  client.write(msg, err => {
-    if (err) {
-      console.error(err.message);
-      return;
-    }
-  });
+  if (message.funcName === "Search") {
+    controllerBySerial[message.serial] = new WgCtl(
+      socket,
+      message.serial,
+      localIp,
+      localPort,
+      message.ip
+    );
+  } else {
+    client.write(msg, err => {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
+    });
+  }
 });
 
-socket.on("listening", () => {
+socket.on("listening", async () => {
   const address = socket.address() as AddressInfo;
   console.log(`[DMN] Socket listening ${address.address}:${address.port}.`);
+  await searchLocalControllers(socket);
+  client.connect(remotePort, remoteHost);
 });
 
 socket.bind(localPort);
-
-client.connect(remotePort, remoteHost);
 
 client.on("connect", () => {
   const address = client.remoteAddress;
   const port = client.remotePort;
   console.log(`[DMN] Socket connected to ${address}:${port}.`);
+  // TODO send local ip to remote server
 });
 
 client.on("close", () => {
@@ -63,20 +75,48 @@ client.on("error", err => {
 });
 
 client.on("data", async data => {
-  console.log(`[DMN] Socket got remote data\n`, data);
+  // console.log(`[DMN] Socket got remote data\n`, data);
   const parsedData = parseData(data);
   const serial = parsedData.serial;
-  // if (!serial) {
-  //   client.destroy(new Error("No controller serial could be parsed."));
-  // }
-  let controller = controllerBySerial[serial];
-  if (!controller) {
-    controller = new WgCtl(socket, parsedData.serial, localIp, localPort);
-    await controller.detected;
-    controllerBySerial[serial] = controller;
+
+  let controller: WgCtl;
+
+  if (!serial) {
+    controller = new WgCtl(socket);
+  } else if (controllerBySerial[serial]) {
+    controller = controllerBySerial[serial];
+  } else {
+    console.error(`[DMN] Controller ${serial} not found, trying re-search.`);
+    new WgCtl(socket).search();
+    return;
   }
-  controller.sendData(data.readUInt8(1), data.slice(8));
+
+  controller.sendData(
+    data.readUInt8(1),
+    Buffer.from(
+      data
+        .slice(8)
+        .toString("hex")
+        .replace(/(00)*$/, ""),
+      "hex"
+    )
+  );
 });
+
+async function searchLocalControllers(socket: UdpSocket) {
+  socket.setBroadcast(true);
+  new WgCtl(socket).search();
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log(
+        `[DMN] Search timeout, controller found:`,
+        Object.keys(controllerBySerial)
+      );
+      socket.setBroadcast(false);
+      resolve();
+    }, 2000);
+  });
+}
 
 // setTimeout(async () => {
 //   const controllers = serials.map(serial => new WgCtl(socket, serial));
